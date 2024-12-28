3d352433ff3344fd0871ca2d1f22e1237be44072
Acronlx <divyaksh1552002@gmail.com>
2024-12-28 10:45:34 +0530

    UI Changes | Added Content

--- apex/src/components/Pages/About/About.tsx
+++ apex/src/components/Pages/About/About.tsx
@@ -284,8 +284,7 @@ function About() {
               <p>
                 Magnam dolores commodi suscipit. Necessitatibus eius consequatur
                 ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam
-                quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea.
-                Quia fugiat sit in iste officiis commodi quidem hic quas.
+                quos quisquam cupiditate. 
               </p>
             </div>
 
